fb66c6eb4760b69a912e25215c1d18136e140aaa
jongwonkim987 <jongwonkim987@gmail.com>
2026-03-17 11:00:15 +0900

    migrate update

--- bookmark/bookmark/models.py
+++ bookmark/bookmark/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Bookmark(models.Model):
+    name = models.CharField('이름', max_length=100)
+    url = models.URLField('URL')
+    created_at = models.DateTimeField('생성일시', auto_now_add=True)
+    updated_at = models.DateTimeField('수정일시', auto_now=True)
+
+    class Meta:
+        verbose_name = '북마크'
+        verbose_name_plural = '북마크 목록'
+
--- bookmark/bookmark/views.py
+++ bookmark/bookmark/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def bookmark_list(request):
+    return render(request, 'bookmark_list.html')
+
+def bookmark_detail(request, number):
+    context = {'number': number}
+    return render(request, 'bookmark_detail.html', context)
