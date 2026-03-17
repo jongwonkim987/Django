190e1b7c725c0be571a699cc4a03a6ecbedf81c5
jongwonkim987 <jongwonkim987@gmail.com>
2026-03-17 18:14:01 +0900

    urls 의존성 수정

--- task-todo/config/urls.py
+++ task-todo/config/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# config/urls.py
 from django.contrib import admin
-from django.urls import path
-from todo.views import todo_list, todo_info
+from django.urls import path, include  # include 추가
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/', todo_list, name='todo_list'),
-    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/', include('todo.urls')),  # todo/urls.py로 위임
 ]
